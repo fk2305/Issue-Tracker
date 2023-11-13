9be51334ed9d6e8ff7bcaa158707bce290ef70ed
fk2305 <thefarhankhan91@gmail.com>
2023-11-13 14:35:31 +0530

    Initial Commit

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const url = 'mongodb://0.0.0.0:27017/Issue-Tracker';
+// mongoose.connect('url') 
+mongoose.connect(url);
+const db=mongoose.connection;
+
+//If any error occuered
+db.on('error',console.error.bind(console,"Error connecting to MongoDB"));   
+
+//On Successfull connection to database
+db.once('open',()=>{
+    console.log("Connected to Database :: MongoDB ")
+});
+
+module.exports=db;  
